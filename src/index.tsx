c4291f10374c11ae11496af8795c720fa35b0b0d
Seongmin Park <seongmin.park@nrise.net>
2024-05-19 12:34:18 +0900

    chore: React Strict Mode 비활성화

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 if (!import.meta.env.VITE_API_KEY) {
   throw new Error('VITE_API_KEY is not defined')
@@ -20,12 +19,10 @@ const queryClient = new QueryClient({
 })
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <HashRouter>
-        <App />
-      </HashRouter>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
-  </React.StrictMode>,
+  <QueryClientProvider client={queryClient}>
+    <HashRouter>
+      <App />
+    </HashRouter>
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>,
 )
