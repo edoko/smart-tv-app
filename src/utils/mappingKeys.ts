d078bd7db932bb6e3322bb0ec76917f84c771e26
Seongmin Park <seongmin.park@nrise.net>
2024-05-19 13:03:57 +0900

    feat: webOS/tizen key 타입 추가

--- src/utils/mappingKeys.ts
+++ src/utils/mappingKeys.ts
@@ -1,28 +1,47 @@
 import getOS from './getOS'
 
-type KeyTypes = 'LEFT' | 'RIGHT' | 'UP' | 'DOWN' | 'ENTER' | 'BACK'
+type KEY_TYPES =
+  | 'LEFT'
+  | 'RIGHT'
+  | 'UP'
+  | 'DOWN'
+  | 'ENTER'
+  | 'PLAY'
+  | 'PAUSE'
+  | 'STOP'
+  | 'FAST-FORWARD'
+  | 'REWIND'
 
-const tvMappingKeys: Record<KeyTypes, number> = {
+const commonMappingKeys: Record<KEY_TYPES, number> = {
   LEFT: 37,
   RIGHT: 39,
   UP: 38,
   DOWN: 40,
   ENTER: 13,
+  PLAY: 415,
+  PAUSE: 19,
+  STOP: 413,
+  'FAST-FORWARD': 417,
+  REWIND: 412,
+}
+const webOSMappingKeys: Record<KEY_TYPES | 'BACK', number> = {
+  ...commonMappingKeys,
   BACK: 461,
 }
-
-const browserMappingKeys: Record<KeyTypes, number> = {
-  LEFT: 37,
-  RIGHT: 39,
-  UP: 38,
-  DOWN: 40,
-  ENTER: 13,
+const tizenMappingKeys: Record<KEY_TYPES | 'BACK', number> = {
+  ...commonMappingKeys,
+  BACK: 10009,
+}
+const browserMappingKeys: Record<KEY_TYPES | 'BACK', number> = {
+  ...commonMappingKeys,
   BACK: 8, // backspace
 }
 
 const mappingKeys =
   process.env.NODE_ENV === 'development' || getOS() === 'browser'
     ? browserMappingKeys
-    : tvMappingKeys
+    : getOS() === 'webOS'
+      ? webOSMappingKeys
+      : tizenMappingKeys
 
 export default mappingKeys
